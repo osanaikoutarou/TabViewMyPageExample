4a31709156bc1af301ef740a5ec979d7d550c497
Kotaro <k.osanai@ashirase.com>
2023-06-21 01:53:05 +0900

    Initial Commit

---
+++ TabViewMyPageExample/TabViewMyPageExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewMyPageExampleApp.swift
+//  TabViewMyPageExample
+//
+//  Created by 長内幸太郎 on 2023/06/21.
+//
+
+import SwiftUI
+
+@main
+struct TabViewMyPageExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
